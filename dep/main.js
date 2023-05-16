1ad6fdc362f85f272b9f60f0da91cd6cfe09877f
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-17 06:18:12 +0900

    onscrollremove

--- dep/main.js
+++ dep/main.js
@@ -9,6 +9,8 @@ let navbar = document.querySelector(".navbar");
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 };
 
 menu.addEventListener("click", () => {
--- dist/index.d5291ba3.js
+++ dist/index.d5291ba3.js
@@ -8,6 +8,8 @@ let navbar = document.querySelector(".navbar");
 window.onscroll = ()=>{
     searchBtn.classList.remove("fa-times");
     searchBar.classList.remove("active");
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
 };
 menu.addEventListener("click", ()=>{
     menu.classList.toggle("fa-times");
