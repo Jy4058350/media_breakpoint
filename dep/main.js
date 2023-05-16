6d22670c8ca916ac45942c4311af8461e824d7e3
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-16 13:05:34 +0900

    clippathactive

--- dep/main.js
+++ dep/main.js
@@ -3,4 +3,5 @@ let searchBar = document.querySelector(".search-bar-container");
 
 searchBtn.addEventListener("click", () => {
     searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
 });
--- dist/index.d5291ba3.js
+++ dist/index.d5291ba3.js
@@ -2,6 +2,7 @@ let searchBtn = document.querySelector("#search-btn");
 let searchBar = document.querySelector(".search-bar-container");
 searchBtn.addEventListener("click", ()=>{
     searchBtn.classList.toggle("fa-times");
+    searchBar.classList.toggle("active");
 });
 
 //# sourceMappingURL=index.d5291ba3.js.map
