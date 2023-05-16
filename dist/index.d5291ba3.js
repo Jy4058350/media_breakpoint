612a41f29a54ef98d2c3e82ace0f664c2494649d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-16 12:56:29 +0900

    fa-timestoggle

--- dist/index.d5291ba3.js
+++ dist/index.d5291ba3.js
@@ -1,2 +1,7 @@
+let searchBtn = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-bar-container");
+searchBtn.addEventListener("click", ()=>{
+    searchBtn.classList.toggle("fa-times");
+});
 
 //# sourceMappingURL=index.d5291ba3.js.map
