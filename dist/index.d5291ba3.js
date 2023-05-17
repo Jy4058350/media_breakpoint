b5e5087c946d777821e7a409888010b1cf958ae4
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-17 13:32:15 +0900

    videocanntchange

--- dist/index.d5291ba3.js
+++ dist/index.d5291ba3.js
@@ -4,8 +4,11 @@ let formBtn = document.querySelector("#login-btn");
 let loginForm = document.querySelector(".login-form-container");
 let formClose = document.querySelector("#form-close");
 let menu = document.querySelector("#menu-bar");
-let navbar = document.querySelector(".navbar");
 let videoBtn = document.querySelectorAll(".vid-btn");
+let navbar = document.querySelector(".navbar");
+//　変更点
+let activeVideo = document.querySelector(".controls .active");
+let videoSlider = document.querySelector("#video-slider");
 window.onscroll = ()=>{
     searchBtn.classList.remove("fa-times");
     searchBar.classList.remove("active");
@@ -29,10 +32,21 @@ formClose.addEventListener("click", ()=>{
 });
 videoBtn.forEach((btn)=>{
     btn.addEventListener("click", ()=>{
-        const active = document.querySelector(".controls .active");
-        active.classList.remove("active");
+        activeVideo.classList.remove("active");
         btn.classList.add("active");
+        let src = btn.getAttribute("data-src");
+        videoSlider.src = src;
+        activeVideo = btn;
     });
-});
+}); // videoBtn.forEach((btn) => {
+ //     btn.addEventListener("click", () => {
+ //       const active = document.querySelector(".controls .active");
+ //       active.classList.remove("active");
+ //       let newMovie = document.querySelector(``);
+ //       btn.classList.add("active");
+ //       let src = btn.getAttribute("data-src");
+ //       document.querySelector("#video-slider").src = src;
+ //     });
+ //   });
 
 //# sourceMappingURL=index.d5291ba3.js.map
